3f1b073793d7ce2cf4496cd08d6c05ac91415c11
liu668 <164828378@qq.com>
2017-04-26 01:12:38 +0800

    4.26

--- before_k_user_sku/user_sku.py
+++ before_k_user_sku/user_sku.py
@@ -3,7 +3,10 @@ Created on 2017年4月24日
 
 @author: liu
 '''
-k=[1,2,3,4,5,6,7]
+action_path = "/home/javis/jd2017/jdata/JData_Action_201604.csv"
+start_date="2016/4/2"
+end_date="2016/4/8"
+for k in [1,2,3,4,5,6,7]
 #该用户前k天浏览多少商品
 before_k_user_brand_type1=
 #该用户前k天加购多少商品
--- rate_feature/rate_feat.py
+++ rate_feature/rate_feat.py
@@ -5,31 +5,24 @@ import numpy as np
 from datetime import date, datetime
 from datetime import timedelta
 
-actions=pd.read_csv("H:/anaconda/JD/JData_Action_201604")
-def get_actions(start_date, end_date):
-    """
 
-    :param start_date:
-    :param end_date:
-    :return: actions: pd.Dataframe
-    """
+action_path = "/home/javis/jd2017/jdata/JData_Action_201604.csv"
+start_date="2016/4/2"
+end_date="2016/4/8"
+actions = pd.read_csv(action_path)
+feature = ['user_id', 'a_rate_total_type4_1', 'a_rate_total_type4_2', 'a_rate_total_type4_3',
+               'a_rate_total_type4_5', 'a_rate_total_type4_6']
+actions = actions[(actions.time >= start_date) & (actions.time < end_date)]
+df = pd.get_dummies(actions['type'], prefix='action')
+actions = pd.concat([actions['user_id'], df], axis=1)
+actions = actions.groupby(['user_id'], as_index=False).sum()
+actions['a_rate_total_type4_1'] = actions['action_4'] / actions['action_1']
+actions['a_rate_total_type4_2'] = actions['action_4'] / actions['action_2']
+actions['a_rate_total_type4_3'] = actions['action_4'] / actions['action_3']
+actions['a_rate_total_type4_5'] = actions['action_4'] / actions['action_5']
+actions['a_rate_total_type4_6'] = actions['action_4'] / actions['action_6']
+actions = actions[feature]
+actions.to_csv("/home/javis/jd2017/jdata/totalRate_feat.csv")
 
-    feature = ['sku_id', 'a_rate_total_type4_1', 'a_rate_total_type4_2', 'a_rate_total_type4_3',
-                'a_rate_total_type4_5', 'a_rate_total_type4_6']
-    actions = get_actions(start_date, end_date)
-    actions = actions[(actions.time >= start_date) & (actions.time < end_date)]
-    df = pd.get_dummies(actions['type'], prefix='action')
-    actions = pd.concat([actions['sku_id'], df], axis=1)
-    actions = actions.groupby(['sku_id'], as_index=False).sum()
-    actions['a_rate_total_type4_1'] = actions['action_4'] / actions['action_1']
-    actions['a_rate_total_type4_2'] = actions['action_4'] / actions['action_2']
-    actions['a_rate_total_type4_3'] = actions['action_4'] / actions['action_3']
-    actions['a_rate_total_type4_5'] = actions['action_4'] / actions['action_5']
-    actions['a_rate_total_type4_6'] = actions['action_4'] / actions['action_6']
-    actions_fet = actions[feature]
-    actions_fet.to_csv("H:/anaconda/JD/data")
-if __name__ == '__main__':
-    start_date="2016/4/2"
-    end_date="2016/4/8"
-    get_actions(start_date, end_date)
+
 
