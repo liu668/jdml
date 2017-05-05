9d06f5538995dd0c1386c4fc87329dd0aed5bfcf
liuwenbin <164828378@qq.com>
2017-05-05 22:18:27 +0800

    model

---
+++ model/quchong.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+pred=pd.read_csv('C:/Users/Administrator/Desktop/forest_result.csv')
+pred=pred.drop_duplicates('user_id')[['user_id','sku_id']]
+pred.to_csv('C:/Users/Administrator/Desktop/forest_result.csv',index=False)
